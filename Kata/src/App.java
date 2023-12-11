de22ed6491d0b80704ec66b7313f3f75ca8b4bca
monk107 <83814865+monk107@users.noreply.github.com>
2023-12-11 20:07:34 +0100

    Add files via upload

---
+++ Kata/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int x=10;
+        int y=25;
+        int z=x+y;
+  
+        System.out.println("Sum of x+y = " + z);
+    }
+}
